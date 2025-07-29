b64d3f07572afd02ee186f50502b079c195845fb
ganeshjadhav9361 <ganeshsjadhav3112@gmail.com>
2025-07-29 12:19:14 +0530

    Added frontend logic

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 let express = require("express");
 let conn = require("../db.js");
+const router = require("./routes/routes.js");
 let path = require("path");
 let bodyParser = require("body-parser");
 let app = express();
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.set("views", path.join(__dirname,"..", "views"));
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/", router); 
 
 module.exports = app;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,6 @@
 let express= require("express");
 let router= express.Router();
-let studCtrl= require(/controllers/studentController.js);
+let studCtrl = require("../controllers/studentController.js");
 
 router.get("/",studCtrl.homePage);
 
