9444e64bdc5d0367b8d87873ee671e8b894a53f1
hariantarkar <harishantarkar3738@gmail.com>
2025-07-29 12:17:45 +0530

    Admin login logic
    Add login controller and login model

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,12 @@
-let express= require("express");
-let router= express.Router();
-let studCtrl= require(/controllers/studentController.js);
+let express = require("express");
+let router = express.Router();
 
-router.get("/",studCtrl.homePage);
+let studCtrl = require("../controllers/studentController.js");
+let loginCtrl = require("../controllers/loginController.js");
 
+router.get("/", studCtrl.homePage);
+router.get("/register", loginCtrl.registerController);
+router.get("/validateuser", loginCtrl.validateLoginUser);
+router.post("/registeruser", loginCtrl.registeruserdata);
 
-
-module.exports= router;
+module.exports = router;
