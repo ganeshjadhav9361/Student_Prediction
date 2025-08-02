970651ea946bb5719207142556e9285945b462ea
ganeshjadhav9361 <ganeshsjadhav3112@gmail.com>
2025-08-02 17:34:45 +0530

    Add file with logic

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -1,8 +1,22 @@
-let express=require("express");
-let router=express.Router();
+const express = require("express");
+const router = express.Router();
+const loginCtrl = require("../controllers/loginController");
+const { verifyToken, requireRole } = require("../middleware/auth.js");
 
-let loginCtrl=require("../controllers/loginController.js");
+router.post("/login", loginCtrl.validateLoginUser);
 
-router.post("/login",loginCtrl.validateLoginUser);
+router.get("/admin/dashboard", verifyToken, requireRole("admin"), (req, res) => {
+    res.render("adminDashboard.ejs", { loginUserName: req.user.username });
+});
 
-module.exports=router
+router.get("/student/dashboard", verifyToken, requireRole("student"), (req, res) => {
+    res.render("studentDashboard.ejs", { loginUserName: req.user.username });
+});
+
+router.get("/logout", (req, res) => {
+    res.clearCookie("token"); 
+    res.render("login.ejs", { msg: "You have been logged out successfully." });
+});
+
+module.exports = router;
+
