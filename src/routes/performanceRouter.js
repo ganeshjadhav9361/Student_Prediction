1a669261053990b5c9dd770f3df71426ca43eda2
ganeshjadhav9361 <ganeshsjadhav3112@gmail.com>
2025-08-16 17:01:41 +0530

    add performance logic

--- src/routes/performanceRouter.js
+++ src/routes/performanceRouter.js
@@ -4,7 +4,7 @@ const performance = require("../controllers/performanceController.js");
 const { verifyToken, isAdmin } = require("../middleware/accessMiddleware.js");
 
 router.post("/performance/view", verifyToken, isAdmin, performance.viewForm);
-//router.post("/performance/add", verifyToken, isAdmin, performance.addPerformance);
+router.post("/performance/add", verifyToken, isAdmin, performance.addPerformance);
 router.post("/performance/update", verifyToken, isAdmin, performance.updatePerformance);
 
 module.exports = router;
